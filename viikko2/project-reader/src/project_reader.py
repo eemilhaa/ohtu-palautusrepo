98b0b708b8ebb7b29e653a27fe2fe84a912f869f
eemilhaa <eemil.haapanen@gmail.com>
2022-11-08 11:49:57 +0200

    deserialize toml with tomli

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,4 +1,5 @@
 from urllib import request
+import tomli
 from project import Project
 
 
@@ -9,7 +10,11 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        toml_dict = tomli.loads(content)
+        name = toml_dict["tool"]["poetry"]["name"]
+        description = toml_dict["tool"]["poetry"]["description"]
+        dependencies = toml_dict["tool"]["poetry"]["dependencies"]
+        dev_dependencies = toml_dict["tool"]["poetry"]["dev-dependencies"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, dependencies, dev_dependencies)
