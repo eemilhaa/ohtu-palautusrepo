42c15ce97e6142a175ca52ed875a4afc5a9e9b8c
eemilhaa <eemil.haapanen@gmail.com>
2022-11-14 13:17:01 +0200

    list all finnish players

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,34 @@
+import requests
 from player import Player
 
+
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict["name"],
+            player_dict["nationality"],
+            player_dict["assists"],
+            player_dict["goals"],
+            player_dict["penalties"],
+            player_dict["team"],
+            player_dict["games"],
+        )
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
