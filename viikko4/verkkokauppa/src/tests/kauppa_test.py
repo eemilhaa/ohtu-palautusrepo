cd889c56e062424fdc047dadfece94a93c7969e0
eemilhaa <eemil.haapanen@gmail.com>
2022-11-23 18:18:56 +0200

    add the rest of tilisiirto tests

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -13,12 +13,22 @@ class TestKauppa(unittest.TestCase):
         self.varasto_mock = Mock()
 
         def varasto_saldo(tuote_id):
-            if tuote_id == 1:
-                return 10
+            saldot = {
+                1: 10,
+                2: 2,
+                3: 3,
+                4: 0,
+            }
+            return saldot[tuote_id]
 
         def varasto_hae_tuote(tuote_id):
-            if tuote_id == 1:
-                return Tuote(1, "maito", 5)
+            tuotteet = {
+                1: Tuote(1, "maito", 5),
+                2: Tuote(2, "juusto", 6),
+                3: Tuote(2, "leipä", 3),
+                4: Tuote(2, "nakki", 10),
+            }
+            return tuotteet[tuote_id]
 
         self.varasto_mock.saldo.side_effect = varasto_saldo
         self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
@@ -34,10 +44,37 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.tilimaksu("pekka", "12345")
         self.pankki_mock.tilisiirto.assert_called()
 
-    def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsuttu_oikeilla_parametreilla(self):
+    def test_yksi_tuote_tilisiirto_kutsuttu_oikeilla_parametreilla(self):
         self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("pekka", "12345")
         self.pankki_mock.tilisiirto.assert_called_with(
             "pekka", 42, "12345", self.kauppa._kaupan_tili, 5
+        )
+
+    def test_kaksi_eri_tuotetta_tilisiirto_kutsuttu_oikeilla_parametreilla(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", self.kauppa._kaupan_tili, 11
+        )
+
+    def test_kaksi_samaa_tuotetta_tilisiirto_kutsuttu_oikeilla_parametreilla(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", self.kauppa._kaupan_tili, 6
+        )
+
+    def test_kaksi_tuotetta_toinen_loppu_tilisiirto_kutsuttu_oikeilla_parametreilla(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.lisaa_koriin(4)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 42, "12345", self.kauppa._kaupan_tili, 3
         )
