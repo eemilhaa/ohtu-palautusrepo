9c0fd62f99758be38e9e3bcbccc64c0eabd75bf6
eemilhaa <eemil.haapanen@gmail.com>
2022-11-23 19:26:30 +0200

    test: test that a new shopping basket is empty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -2,9 +2,12 @@ import unittest
 from ostoskori import Ostoskori
 from tuote import Tuote
 
+
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
+        ostosten_maara = len(self.kori.ostokset())
+        self.assertEqual(ostosten_maara, 0)
