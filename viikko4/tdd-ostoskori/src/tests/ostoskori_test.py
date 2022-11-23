935dc92fe5b1723cdb334df8d27c8eacfcd55329
eemilhaa <eemil.haapanen@gmail.com>
2022-11-23 19:40:11 +0200

    fix: properly test the number of products in the basket

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -9,5 +9,4 @@ class TestOstoskori(unittest.TestCase):
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
-        ostosten_maara = len(self.kori.ostokset())
-        self.assertEqual(ostosten_maara, 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
