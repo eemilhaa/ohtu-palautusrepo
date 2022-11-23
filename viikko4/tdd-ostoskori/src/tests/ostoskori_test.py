10f04c6b33a0baa46f627139e785670a4b5a58c6
eemilhaa <eemil.haapanen@gmail.com>
2022-11-23 20:10:30 +0200

    test: test basket price with 1 product

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -15,3 +15,9 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_oikea_hinta_yhden_tuotteen_lisaamisen_jalkeen(self):
+        HINTA = 2
+        leipa = Tuote("Leipä", HINTA)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(), HINTA)
