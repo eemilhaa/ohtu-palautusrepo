bf923a41e492234ad39bde16e252a9ec96768694
eemilhaa <eemil.haapanen@gmail.com>
2022-11-24 11:44:40 +0200

    refactor: clean up ostoskori

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,44 +4,34 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         self._ostokset = []
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        maara = 0
-        for ostos in self._ostokset:
-            maara += ostos.lukumaara()
-        return maara
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        return sum([ostos.lukumaara() for ostos in self._ostokset])
 
     def hinta(self):
-        hinta = 0
-        for ostos in self._ostokset:
-            hinta += ostos.hinta()
-        return hinta
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        return sum([ostos.hinta() for ostos in self._ostokset])
 
     def lisaa_tuote(self, lisattava: Tuote):
-        for ostos in self._ostokset:
-            if lisattava.nimi() == ostos.tuotteen_nimi():
-                ostos.muuta_lukumaaraa(1)
-                return
-        self._lisaa_uusi_ostos(lisattava)
+        ostoksen_lukumaara = self._muuta_ostoksen_lukumaaraa(lisattava, 1)
+        if not ostoksen_lukumaara:
+            self._lisaa_uusi_ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
-        for ostos in self._ostokset:
-            if poistettava.nimi() == ostos.tuotteen_nimi():
-                ostos.muuta_lukumaaraa(-1)
-        self._poista_tyhjat_ostokset()
+        ostoksen_lukumaara = self._muuta_ostoksen_lukumaaraa(poistettava, -1)
+        if ostoksen_lukumaara < 1:
+            self._poista_tyhjat_ostokset()
 
     def tyhjenna(self):
         self._ostokset = []
 
     def ostokset(self):
         return self._ostokset
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+
+    def _muuta_ostoksen_lukumaaraa(self, tuote: Tuote, muutos: int):
+        for ostos in self._ostokset:
+            if tuote.nimi() == ostos.tuotteen_nimi():
+                ostos.muuta_lukumaaraa(muutos)
+                return ostos.lukumaara()
 
     def _lisaa_uusi_ostos(self, lisattava: Tuote):
         ostos = Ostos(lisattava)
