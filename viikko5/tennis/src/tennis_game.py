e1b98a9fd5de6435ba05b2d997d4d2a37f4b76d5
eemilhaa <eemil.haapanen@gmail.com>
2022-11-28 17:09:37 +0200

    refactor: better variable names

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,57 +1,58 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1 = player1_name
+        self.player2 = player2_name
+        self.score1 = 0
+        self.score2 = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.score1 = self.score1 + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.score2 = self.score2 + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        output = ""
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
+        if self.score1 == self.score2:
+            if self.score1 == 0:
+                output = "Love-All"
+            elif self.score1 == 1:
+                output = "Fifteen-All"
+            elif self.score1 == 2:
+                output = "Thirty-All"
+            elif self.score1 == 3:
+                output = "Forty-All"
             else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+                output = "Deuce"
+
+        elif self.score1 >= 4 or self.score2 >= 4:
+            minus_result = self.score1 - self.score2
 
             if minus_result == 1:
-                score = "Advantage player1"
+                output = "Advantage player1"
             elif minus_result == -1:
-                score = "Advantage player2"
+                output = "Advantage player2"
             elif minus_result >= 2:
-                score = "Win for player1"
+                output = "Win for player1"
             else:
-                score = "Win for player2"
+                output = "Win for player2"
+
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.score1
                 else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+                    output = output + "-"
+                    temp_score = self.score2
 
                 if temp_score == 0:
-                    score = score + "Love"
+                    output = output + "Love"
                 elif temp_score == 1:
-                    score = score + "Fifteen"
+                    output = output + "Fifteen"
                 elif temp_score == 2:
-                    score = score + "Thirty"
+                    output = output + "Thirty"
                 elif temp_score == 3:
-                    score = score + "Forty"
+                    output = output + "Forty"
 
-        return score
+        return output
